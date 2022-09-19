373eff75a2b21efb660ca584e7a0798aa9634d98
Lukasz Gut <40406905+Blinkuu@users.noreply.github.com>
2022-09-19 18:50:01 +0200

    refactor: unify naming across services and handlers (#27)

--- internal/core/ports/services.go
+++ internal/core/ports/services.go
@@ -9,11 +9,11 @@ type PingService interface {
 	Ping(ctx context.Context) string
 }
 
-type RateQuotaService interface {
+type RateService interface {
 	Allow(ctx context.Context, namespace, resource string, tokens int64) (time.Duration, bool, error)
 }
 
-type AllocationQuotaService interface {
+type AllocService interface {
 	Alloc(ctx context.Context, namespace, resource string, tokens int64) (int64, bool, error)
 	Free(ctx context.Context, namespace, resource string, tokens int64) (int64, bool, error)
 }
