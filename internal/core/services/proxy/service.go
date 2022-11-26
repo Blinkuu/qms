204872ad4ce36a0403225e63694c160b936873e3
Lukasz Gut <blinku.dev@gmail.com>
2022-11-26 21:00:58 +0100

    feat: support virtual nodes in hash ring

--- internal/core/services/proxy/service.go
+++ internal/core/services/proxy/service.go
@@ -66,13 +66,12 @@ func (s *Service) Allow(ctx context.Context, namespace, resource string, tokens 
 	s.rateMu.RLock()
 	defer s.rateMu.RUnlock()
 
-	id := strings.Join([]string{namespace, resource}, "_")
-	addr, ok := s.rateHashRing.GetNode(id)
-	if !ok {
-		return 0, false, fmt.Errorf("failed to get address from ring: id=%s", id)
+	addrs, err := s.hashRingLocked(namespace, resource)
+	if err != nil {
+		return 0, false, fmt.Errorf("failed to pick addresses from hash ring: %w", err)
 	}
 
-	return s.rateClient.Allow(ctx, []string{addr}, namespace, resource, tokens)
+	return s.rateClient.Allow(ctx, addrs, namespace, resource, tokens)
 }
 
 func (s *Service) View(ctx context.Context, namespace, resource string) (int64, int64, int64, error) {
@@ -152,10 +151,12 @@ func (s *Service) roundRobinLocked() []string {
 
 func (s *Service) hashRingLocked(namespace, resource string) ([]string, error) {
 	id := strings.Join([]string{namespace, resource}, "_")
-	addr, ok := s.rateHashRing.GetNode(id)
+	vNode, ok := s.rateHashRing.GetNode(id)
 	if !ok {
 		return nil, fmt.Errorf("failed to get address from ring: id=%s", id)
 	}
+
+	addr := s.trimVNodePrefix(vNode)
 
 	return []string{addr}, nil
 }
@@ -197,14 +198,14 @@ func (s *Service) updateRings() {
 	if err != nil {
 		s.logger.Warn("failed to get rate members", "err", err)
 	} else {
-		s.updateRateMembersAndHashRing(members)
+		s.updateRateMembersAndHashRing(members, 10)
 	}
 
 	members, err = s.fetchMembers(s.cfg.AllocAddresses)
 	if err != nil {
 		s.logger.Warn("failed to get alloc members", "err", err)
 	} else {
-		s.updateAllocMembersAndHashRing(members)
+		s.updateAllocMembersAndHashRing(members, 10)
 	}
 }
 
@@ -225,12 +226,16 @@ func (s *Service) fetchMembers(discoverAddrs []string) ([]domain.Instance, error
 	return members, nil
 }
 
-func (s *Service) updateRateMembersAndHashRing(newMembers []domain.Instance) {
+func (s *Service) updateRateMembersAndHashRing(newMembers []domain.Instance, numVNodes int) {
 	s.logger.Info("updating rate hash ring", "new_members", fmt.Sprintf("%+v", newMembers))
 
 	nodes := make([]string, 0, len(newMembers))
 	for _, member := range newMembers {
-		nodes = append(nodes, fmt.Sprintf("%s:%d", member.Host, member.HTTPPort))
+		for i := 0; i < numVNodes; i++ {
+			addr := fmt.Sprintf("%s:%d", member.Host, member.HTTPPort)
+			vNode := s.prependVNodePrefix(addr, i)
+			nodes = append(nodes, vNode)
+		}
 	}
 
 	s.rateMu.Lock()
@@ -240,12 +245,16 @@ func (s *Service) updateRateMembersAndHashRing(newMembers []domain.Instance) {
 	s.rateHashRing = hashring.New(nodes)
 }
 
-func (s *Service) updateAllocMembersAndHashRing(newMembers []domain.Instance) {
+func (s *Service) updateAllocMembersAndHashRing(newMembers []domain.Instance, numVNodes int) {
 	s.logger.Info("updating alloc hash ring", "new_members", fmt.Sprintf("%+v", newMembers))
 
 	nodes := make([]string, 0, len(newMembers))
 	for _, member := range newMembers {
-		nodes = append(nodes, fmt.Sprintf("%s:%d", member.Host, member.HTTPPort))
+		for i := 0; i < numVNodes; i++ {
+			addr := fmt.Sprintf("%s:%d", member.Host, member.HTTPPort)
+			vNode := s.prependVNodePrefix(addr, i)
+			nodes = append(nodes, vNode)
+		}
 	}
 
 	s.allocMu.Lock()
@@ -253,4 +262,16 @@ func (s *Service) updateAllocMembersAndHashRing(newMembers []domain.Instance) {
 
 	s.allocMembers = newMembers
 	s.allocHashRing = hashring.New(nodes)
+}
+
+func (s *Service) prependVNodePrefix(addr string, vNodeID int) string {
+	return fmt.Sprintf("vnode%d_%s", vNodeID, addr)
+}
+
+func (s *Service) trimVNodePrefix(vNode string) string {
+	before, addr, ok := strings.Cut(vNode, "_")
+
+	s.logger.Info("trimVNodePrefix", "before", before, "addr", addr, "ok", ok)
+
+	return addr
 }
